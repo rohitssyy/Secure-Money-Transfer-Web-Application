03adf8ac08fc6af390e464abbc745a0f6628b8b8
rohitssyy <rohitssyy@gmail.com>
2025-05-24 05:28:08 +0530

    minor change v5

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signin from "./pages/signin";
-import Signup from "./pages/signup";
-import Dashboard from "./pages/dashboard";
-import Sendmoney from "./pages/sendmoney";
+import Signin from "./pages/signin.jsx";
+import Signup from "./pages/signup.jsx";
+import Dashboard from "./pages/dashboard.jsx";
+import Sendmoney from "./pages/sendmoney.jsx";
 
 function App() {
   return (
