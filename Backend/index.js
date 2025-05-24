8d4fdcdb87333452ee6561ef8a038055f57efc32
rohitssyy <rohitssyy@gmail.com>
2025-05-25 00:45:40 +0530

    hopefully the final change

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,23 @@ const cors = require("cors");
 const userRouter = require("./routes/user")
 const accountRouter = require("./routes/account");
 
-app.use(cors());
+const allowedOrigins = ["http://localhost:5173/" , "https://secure-money-transfer-web-application.vercel.app/"]
+
+app.use(cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin like mobile apps or curl
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  }));
+
+
+
 app.use(express.json())
 
 app.use("/api", userRouter)
