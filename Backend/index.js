e1a7765ee713c654d3efd289347be941cf45caa4
rohitssyy <rohitssyy@gmail.com>
2025-05-25 01:22:53 +0530

    configured cors

--- Backend/index.js
+++ Backend/index.js
@@ -9,12 +9,23 @@ const allowedOrigins = ["http://localhost:5173/",
   "https://secure-money-transfer-web-application-55vin1866.vercel.app",
   "https://secure-money-transfer-web-a-git-d40487-rohits-projects-faa774b9.vercel.app"]
 
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  };
+
 
 app.use(cors({
   origin: allowedOrigins,
   credentials: true
 }))
-
+app.use(cors(corsOptions));
 
 
 app.use(express.json())
