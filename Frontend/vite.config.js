5214785de6bae5124fdeab4a15f71113effcd1d6
rohitssyy <rohitssyy@gmail.com>
2025-05-24 23:37:34 +0530

    vite config changed

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
         tailwindcss(),
-
-  ],
+      ],
+      base: import.meta.env.VITE_BASE_PATH || "/Secure-Money-Transfer-Web-Application/tree/main/Frontend"
 })
