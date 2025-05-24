e5a80b870c4c465ccd30e77aecf1ce3f99de4ae2
rohitssyy <rohitssyy@gmail.com>
2025-05-24 23:41:09 +0530

    vite config remove

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
   plugins: [react(),
         tailwindcss(),
       ],
-      base: import.meta.env.VITE_BASE_PATH || "/Secure-Money-Transfer-Web-Application/tree/main/Frontend"
+      
 })
